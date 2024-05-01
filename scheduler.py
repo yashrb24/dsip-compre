dd8e32285af1065abdce9ad7afb9894925ede4ed
yashrb24 <bhisikaryash2403@gmail.com>
2024-05-01 20:44:06 +0530

    major changes

--- scheduler.py
+++ scheduler.py
@@ -1,19 +1,24 @@
-# scheduler listens for input on port 5001 and computes the sum of numbers from 1 to the input number in a for loop and displays the result on the browser
+import asyncio
+import websockets
+from flask import Flask, request
 
-
-import requests
-from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+ports = [8500, 8501, 8502, 8503, 8504, 8505]
+current_port_idx = 0
+
+async def send_messages(number):
+    global current_port_idx
+    async with websockets.connect(f"ws://localhost:{ports[current_port_idx]}") as websocket:
+        await websocket.send(str(number))
+        current_port_idx  = (1 + current_port_idx) % len(ports)
 
 @app.route('/sum', methods=['POST'])
 def sum():
+    global current_port_idx
     number = int(request.form['number'])
-    total = 0
-    for i in range(1, number + 1):
-        total += i
-
-    return jsonify({'result': total})
+    asyncio.run(send_messages(number))  # Run the coroutine in the event loop
+    return f"Task scheduled at worker{current_port_idx}. Go back to the homepage to schedule another task."
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(port=5001, debug=True)  # Run the Flask app
